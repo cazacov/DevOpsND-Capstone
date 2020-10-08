01adb77c5706ae650521210a859c2a231aebf064
Victor Cazacov <cazacov.git@gmail.com>
2020-10-08 02:22:19 +0200

    Use EKS:Nodegroup

--- Kubernetes/02-create-network.sh
+++ Kubernetes/02-create-network.sh
@@ -3,4 +3,5 @@
 aws cloudformation create-stack \
     --stack-name udacity-devops-net \
     --template-body file://02-amazon-eks-vpc-private-subnets.yml \
+    --parameters file://params-cluster.json \
     --region us-west-2 
