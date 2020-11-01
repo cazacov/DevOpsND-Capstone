b3080fc9a22196b2becf15a451d359d8bd80380f
Victor Cazacov <cazacov.git@gmail.com>
2020-11-01 16:36:56 +0100

    add Docker-Hub deployment

---
+++ webapp/run_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=capstone .
+
+# Step 2: 
+# List docker images
+docker image ls cpstone*
+
+# Step 3: 
+# Run interactive shell
+#docker run -it mlk8s bash
+
+# Step 3: 
+# Run flask app
+docker run -p 80:80 capstone
---
+++ webapp/upload_docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=cazacov/learning:capstone
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+
+# Authenticate
+docker login -u cazacov
+
+# Tag
+docker tag capstone $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
